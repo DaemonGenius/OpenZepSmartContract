445426a9d137820e633ac45b5c41bf276365628b
Christian <christian.jhardy.steinmann@gmail.com>
2022-02-28 22:02:22 +0200

    Added Steinnegen Coin to token sale

--- contracts/SteinnegenSale.sol
+++ contracts/SteinnegenSale.sol
@@ -1,17 +1,18 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
-import "./Coin.sol";
-import "../node_modules/openzeppelin-solidity/contracts/utils/math/SafeMath.sol";
 
-contract CoinSale {
+import "openzeppelin-solidity/contracts/utils/math/SafeMath.sol";
+import "@DaemonGenius/ico/ico-steinnegen-coin/contracts/Steinnegen.sol";
+
+contract SteinnegenSale {
     address payable private admin;
-    Coin public tokenContract;
+    Steinnegen public tokenContract;
     uint256 public tokenPrice;
     uint256 public tokensSold;
 
     event Sell(address _buyer, uint256 _amount);
 
-    constructor(Coin _tokenContract, uint256 _tokenPrice) {
+    constructor(Steinnegen _tokenContract, uint256 _tokenPrice) {
         admin = payable(msg.sender);
         tokenContract = _tokenContract;
         tokenPrice = _tokenPrice;
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,11 +1,11 @@
-const Coin = artifacts.require("./Coin.sol");
-const CoinSale = artifacts.require("./CoinSale.sol");
+const Steinnegen = artifacts.require("@DaemonGenius/ico/ico-steinnegen-coin/contracts/Steinnegen.sol");
+const SteinnegenSale = artifacts.require("./SteinnegenSale.sol");
 
 module.exports = async (deployer) => {
   let tokenPrice = 1000000000000000;
 
-  await deployer.deploy(Coin);
-  await deployer.deploy(CoinSale, Coin.address, tokenPrice);
+  await deployer.deploy(Steinnegen);
+  await deployer.deploy(SteinnegenSale, Steinnegen.address, tokenPrice);
 }; 
 
 
